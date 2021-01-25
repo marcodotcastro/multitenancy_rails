2e5cce36702cbb6c0bd335cf103c54ec86ebb533
Marco Castro <marcodotcastro@gmail.com>
2021-01-25 11:34:04 -0300

    feat(articles): add crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_143247) do
+ActiveRecord::Schema.define(version: 2021_01_25_143345) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "slug"
